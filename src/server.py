0359055a54e7b65bd14478b0f18fa669b17656c9
inux <inux.steve@gmail.com>
2019-03-09 01:26:00 +0100

    first working version

--- src/server.py
+++ src/server.py
@@ -11,10 +11,12 @@ from sanic import Sanic
 from sanic.response import json
 from sanic.response import file
 
+import helper.log_helper as logger
 import config
 
 app = Sanic()
 app.name = "CrashSimulationAsimov"
+log = logger.get(True, "Server")
 
 # SIGINT handler (when pressing Ctrl+C)
 
@@ -27,12 +29,14 @@ def signal_int_handler(sig, frame):
 @app.route('/api/v1/getCrashInfo', methods=['POST',])
 async def crash_info(request):
     ''' crash info parses the crash record and returns a JSON object '''
+    log.info("Handling '/api/v1/getCrashInfo'")
     return json({'impactAngle': 90, 'offsetMaximumForce': 3})
 
 # POST request 2 - returns a rendered crash image (PNG)
 @app.route('/api/v1/getCrashImage', methods=['POST',])
 async def crash_image(request):
     ''' crash image parses the crash record and returns a JSON object '''
+    log.info("Handling '/api/v1/getCrashImage'")
     return await file('images/lena.png')
 
 if __name__ == '__main__':
--- src/test.py
+++ src/test.py
@@ -1,19 +1,26 @@
 '''
 Usage:
-python test.py -cinf=True -cimg=True -d=data/1.json
+- run server (python server.py)
+- run test (python test.py -cinf=True -cimg=True -d=data/1.json)
 '''
 import helper.log_helper as logger
 
+import requests
 import argparse
 import config
 
-def test_crashinfo(**kwargs):
-    log = logger.get(True, "Main")
-    log.info("START hack winners 2019")
+def test_crashinfo(json_file):
+    __post_request(__get_url('/api/v1/getCrashInfo'), json_file)
 
-def test_crashinfo(**kwargs):
-    log = logger.get(True, "Main")
-    log.info("START hack winners 2019")
+def test_crashimage(json_file):
+    __post_request(__get_url('/api/v1/getCrashImage'), json_file)
+
+def __post_request(url, json_file):
+    resp = requests.post(url, data=open(json_file, 'rb'))
+    return resp
+
+def __get_url(endpoint):
+    return 'http://'+config.host+':'+str(config.port)+endpoint
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test utility for CrashSimulationAsimov')
@@ -25,10 +32,12 @@ if __name__ == "__main__":
     log = logger.get(True, "TestUtility")
     if args['crashInfo'] == "True":
         log.info("Executing crashInfo test with: " + args['data'])
+        test_crashinfo(args['data'])
     else:
         log.info("CrashInfo test not enabled...")
 
     if args['crashImage'] == "True":
         log.info("Executing crashImage test with: " + args['data'])
+        test_crashimage(args['data'])
     else:
         log.info("crashImage test not enabled...")
