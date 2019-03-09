4be7e517b90b3ab1384e18cd8da81bb41fb20bc1
Serge Hauri <sergehauri@gmail.com>
2019-03-09 17:15:01 +0100

    Refactoring

--- src/data_parser.py
+++ src/data_parser.py
@@ -4,61 +4,50 @@ import base64
 import json
 import math
 
-from helper import log_helper
-#Definde Logger without Filehandle
+from .helper import log_helper
+
+# Define Logger without file handle
 logger = log_helper.get(False, "Data Parse")
 
-
-
 class DataParser:
-    """Gibt Winkel und maxforce zurück
-
-    Arguments:
-        file_path - Pfad zum JSON
-        direct_json - direkt JSON übergeben als alternative {default: False}
-
-    Returns:
-        [angle_impatc, max_force_offset, max_force, forces_list] -- [winkel in Grad, Offset zum Max Force in ms, Max Force relativ zu G, Liste allte Kräfte mit timestamp]
-    """
-
     def parse_input_data(self, file_path, calibration=True):
-        basejson = self.__read_json_from_filesystem(file_path)
-        b64payload = self.__get_b64payload_from_basejson(basejson)
+        """
+        Parses a JSON
+        :param file_path:   path to the JSON file
+        :param calibration: if True, acceleration data will be calibrated
+        :return:    (angle in degrees, offset to max force value in ms, relative max force, list of all force values)
+        """
+        base_json = self.__read_json_from_filesystem(file_path)
+        b64payload = self.__get_b64payload_from_basejson(base_json)
 
         encoded = self.__base64_decode(b64payload)
-        #convert to python list
+        # convert to python list
         pylist = self.__encoded_payload_to_list(encoded)
 
         predicted_impact_time = pylist["data"][pylist["pos"]][0]
 
-        #pylist['data'] = self.__convert_timestamps(pylist['data'], pylist['timestamp'], pylist['referenceTime'])
-
         acceleration = pylist["data"]
         acceleration = sorted(acceleration, key=lambda d: d[0])
 
-        #Do the calibration
         if calibration:
-            acceleration= self.__get_virtual_xyz(pylist['data'], pylist['calibration'])
-
-        #norm with oneG
-        oneG = pylist["oneG"]
+            acceleration = self.__get_virtual_xyz(pylist['data'], pylist['calibration'])
 
         rel_time = [x[0] for x in acceleration]
         rx = [x[1] for x in acceleration]
         ry = [x[2] for x in acceleration]
         rz = [x[3] for x in acceleration]
-        #calculate forces
-        forces = self.__calculate_forces(rx,ry,rz)
-        max_force_offset = self.__calculate_offset_max_force(rel_time, rx,ry,rz)
-        max_force = self.__calculate_max_force(rel_time, rx,ry,rz)
 
-        #calculate angle
-        angle_impatc = self.__calculate_angle(max_force_offset, predicted_impact_time,rel_time, rx,ry)
+        # calculate forces
+        forces = self.__calculate_forces(rx, ry, rz)
+        max_force_offset = self.__calculate_offset_max_force(rel_time, rx, ry, rz)
+        max_force = self.__calculate_max_force(rel_time, rx, ry, rz)
 
-        forces_list = [rel_time,forces]
+        # calculate angle
+        angle_impact = self.__calculate_angle(max_force_offset, predicted_impact_time, rel_time, rx, ry)
 
-        return (angle_impatc, max_force_offset, max_force, forces_list)
+        forces_list = [rel_time, forces]
 
+        return angle_impact, max_force_offset, max_force, forces_list
 
     def __base64_decode(self, base64_string):
         """
@@ -88,57 +77,46 @@ class DataParser:
         array[:, 0] = array[:, 0] + c
         return array.tolist()
 
-
     def __get_virtual_xyz(self, acceleration, calibration):
         """
-        Calibrates virutal x,y,z data from the acceleration and calibration (0 used)
+        Calibrates virtual x,y,z data from the acceleration and calibration (0 used)
 
         :param acceleration:    data array with [ [timestamp, x, y, z], [...], ...]
         :param calibration:     calibrations array like [ [x,y,z] , [x2,y2,z2], [x3,y3,z3] ]
         :return:                Virtual position array like [ [timestamp, virt_x, virt_y, virt_z], ...]
         """
-        #korrekt
         for i_acc in acceleration:
             virt_x = calibration[0][0] * i_acc[1] + calibration[0][1] * i_acc[2] + calibration[0][2] * i_acc[3]
             i_acc[1] = virt_x
-            #logger.info("x: %d",virt_x)
 
-            virt_y = calibration[1][0] * i_acc[1] + calibration[1][1] * i_acc[2]  + calibration[1][2] * i_acc[3]
+            virt_y = calibration[1][0] * i_acc[1] + calibration[1][1] * i_acc[2] + calibration[1][2] * i_acc[3]
             i_acc[2] = virt_y
-            #logger.info("y: %d",virt_y)
 
-            virt_z = calibration[2][0] * i_acc[1] + calibration[2][1] * i_acc[2]  + calibration[2][2] * i_acc[3]
+            virt_z = calibration[2][0] * i_acc[1] + calibration[2][1] * i_acc[2] + calibration[2][2] * i_acc[3]
             i_acc[3] = virt_z
-            #logger.info("z: %d",virt_z)
 
         return acceleration
 
-
-    def __norm_with_g(self, acceleration,  oneG):
+    def __norm_with_g(self, acceleration, one_g):
         for n_acc in acceleration:
-            n_acc[1] = n_acc[1] / oneG
-            n_acc[2] = n_acc[2] / oneG
-            n_acc[3] = n_acc[3] / oneG
+            n_acc[1] = n_acc[1] / one_g
+            n_acc[2] = n_acc[2] / one_g
+            n_acc[3] = n_acc[3] / one_g
         return acceleration
 
-
-    #only force calc
+    # only force calc
     def __calculate_forces(self, rx, ry, rz):
-        return [np.sqrt(x**2 + y**2 + z**2) for x, y, z in zip(rx,ry,rz)]
+        return [np.sqrt(x ** 2 + y ** 2 + z ** 2) for x, y, z in zip(rx, ry, rz)]
 
-
-
-    #not directly
-    def __calculate_max_force(self, rel_time, rx,ry,rz):
-        forces = self.__calculate_forces(rx,ry,rz)
+    # not directly
+    def __calculate_max_force(self, rel_time, rx, ry, rz):
+        forces = self.__calculate_forces(rx, ry, rz)
         return np.max(forces)
 
-
-    def __calculate_offset_max_force(self, rel_time, rx,ry,rz):
-        forces = self.__calculate_forces(rx,ry,rz)
-        offset_max_force = forces.index(np.max(forces)) #liefert den index an dem force maximal ist
-        return rel_time[offset_max_force] #liefer rel time (offset) an der force maximal ist
-
+    def __calculate_offset_max_force(self, rel_time, rx, ry, rz):
+        forces = self.__calculate_forces(rx, ry, rz)
+        offset_max_force = forces.index(np.max(forces))  # index of max force value
+        return rel_time[offset_max_force]  # relative time at max force value
 
     def __calculate_angle(self, offset_maxforce_in_ms, predicted_impact_time, rel_time, rx, ry):
         try:
@@ -158,7 +136,6 @@ class DataParser:
         :param ringbuffer:  raw data from the accelerometer component
         :return:            data sorted by timestamp
         """
-        # TODO implement SERGE
         rb = np.array(ringbuffer)
         return rb[rb[:, 0].argsort()]
 
@@ -167,20 +144,17 @@ class DataParser:
             with open(path2file) as json_file:
                 data = json.load(json_file)
                 return data
-
         if type(path2file) is str:
-            return json.loads(path2file) #json was passed as str
-
+            return json.loads(path2file)  # json was passed as str
         return None
 
-    def __get_b64payload_from_basejson(self, basejson):
-        print(type(basejson))
-        return basejson[0]['payload']['b64_payload']
+    def __get_b64payload_from_basejson(self, base_json):
+        return base_json[0]['payload']['b64_payload']
 
     def __encoded_payload_to_list(self, encodedjsonstring):
         return json.loads(encodedjsonstring)
 
-#testing
-#dp = DataParser()
-#result = dp.parse_input_data(r'C:\hslu\git\starthack-asimov\src\data\1.json')
-#print(result)
+# testing
+# dp = DataParser()
+# result = dp.parse_input_data(r'C:\hslu\git\starthack-asimov\src\data\1.json')
+# print(result)
