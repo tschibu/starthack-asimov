741ac5f5118855f0d858d20134bc5b08118f749c
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-03-09 05:12:29 +0100

    new version

--- src/drawer.py
+++ src/drawer.py
@@ -1,26 +1,39 @@
 import cv2
 import helper.log_helper as logger
+import numpy as np
+import os
 
 class Drawer:
     def __init__(self, x, y, radius, angle, off_set_in_milliseconds=None):
-        self.image = cv2.imread("image/car_big.png")
+        self.image = cv2.imread("images/car_big.png")
         self.x = x
         self.y = y
         self.radius = radius
         self.angle = angle
         self.off_set_in_milliseconds = off_set_in_milliseconds
+
+        self.image_path = "images/car_rendered_at_off_set_in_milliseconds.png"
+
         self.log = logger.get(True, "Drawer")
         self.log.info(
             "Param x=" + str(self.x) + "; y=" + str(self.y) + "; radius=" + str(self.radius) + "; angle=" + str(
                 self.angle))
         self.log.debug("Image-shape = " + str(self.image.shape))
 
+    def __cut_car(self):
+        print("TODO")
+        # TODO: u need to do this
+
     def __draw(self):
         if self.off_set_in_milliseconds is not None:
-            self.__add_text(123)
+            self.__add_text(self.off_set_in_milliseconds)
+        self.__draw_arrow()
+        self.__draw_circle()
 
     def __draw_arrow(self):
-        cv2.arrowedLine(self.image, (1457, 652), (self.x, self.y), (0, 0, 255), 4)
+        # TODO: calculate source from angle
+
+        cv2.arrowedLine(self.image, (1457, 1208), (self.x, self.y), (0, 0, 255), 4)
 
     def __draw_circle(self):
         cv2.circle(self.image, (self.x, self.y), self.radius, (0, 0, 255), 2)
@@ -38,12 +51,20 @@ class Drawer:
                     font_scale,
                     font_color,
                     line_type)
-        self.log.info("add off_set" + str(off_set_in_milliseconds))
+        self.log.info("add off_set=" + str(off_set_in_milliseconds) + " to the image.")
+
+    def __write_image(self):
+        cv2.imwrite(self.image_path, self.image)
 
     def get_image(self):
         """Retrun a image with the a circle and an arrow.
         """
         self.__draw()
+        self.__write_image()
+        return self.image_path
+
+    def remove_image(self):
+        os.remove(self.image_path)
 
     def show_image(self):
         """show the image with the right circle and arrow with opencv.
@@ -61,5 +82,7 @@ class Drawer:
 
 
 if __name__ == "__main__":
-    drawer = Drawer(1300, 550, 60, 180)
-    drawer.show_image()
+    drawer = Drawer(1300, 550, 60, 180, 6110)
+    print(drawer.get_image())
+    drawer.remove_image()
+
