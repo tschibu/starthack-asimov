1b52928354f837b7fe2f9ae70646cb33c9aef6bc
Dave <david.schafer@stud.hslu.ch>
2019-03-09 01:31:48 +0100

    Base64 encoder and Logging implemented

--- src/data_parser.py
+++ src/data_parser.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
+import base64
+
+from helper import log_helper
+#Definde Logger without Filehandle
+logger = log_helper.get(False, "Data Parse")
+
+
 
 class DataParser:
     def parse_input_data(self, data):
@@ -10,10 +17,17 @@ class DataParser:
     def __base64_decode(self, base64_string):
         """
         Decodes a base64 encoded string to a UTF-8 string.
+
         :param base64_string:    base64 string
-        :return:                    decoded character string
+        :return:                 decoded character string
         """
-        # TODO implement DAVE
+        logger.info("Base64 Encoding string")
+
+        #Decode string
+        decoded_string = base64.b64decode(base64_string)
+        decoded_string = decoded_string.decode("utf-8")
+
+        return decoded_string
 
     def __relative2absolute_timestamp(self, relative_timestamp, timestamp, reference_time):
         """
@@ -43,3 +57,4 @@ class DataParser:
         """
         # TODO implement SERGE
 
+        
